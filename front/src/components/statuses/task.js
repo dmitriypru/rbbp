18ef28bc96809dfec3a5540ede875de4a50b3e7f
fargothcor <levchenya.roman@gmail.com>
2019-11-16 13:12:57 +0300

    Added working front
    Finally, some good front

--- front/src/components/statuses/task.js
+++ front/src/components/statuses/task.js
@@ -3,35 +3,43 @@ import '../board/board.css';
 
 class Task extends Component {
     render() {
-        if (this.props.state === 'up'){
+        if (this.props.state === 101){
         return (
             <div class='task_up'>
                 <h3 class="h">{this.props.name}</h3>
                 <h4 class="h">Flags: {this.props.flags} <br/> Uptime: {this.props.uptime}</h4>
             </div>
         )}
-        if (this.props.state === 'mumble'){
+        if (this.props.state === 103){
             return (
                 <div class='task_mumble'>
                     <h3 class="h">{this.props.name}</h3>
                     <h4 class="h">Flags: {this.props.flags} <br/> Uptime: {this.props.uptime}</h4>
                 </div>
             )}
-        if (this.props.state === 'corrupt'){
+        if (this.props.state === 102){
             return (
                 <div class='task_corrupt'>
                     <h3 class="h">{this.props.name}</h3>
                     <h4 class="h">Flags: {this.props.flags} <br/> Uptime: {this.props.uptime}</h4>
                 </div>
             )}
-        if (this.props.state === 'down'){
+        if (this.props.state === 104){
                 return (
                     <div class='task_down'>
                         <h3 class="h">{this.props.name}</h3>
                         <h4 class="h">Flags: {this.props.flags} <br/> Uptime: {this.props.uptime}</h4>
                     </div>
                 )}
+        else{
+            return (
+                <div class='task_down'>
+                    <h3 class="h">{this.props.name}</h3>
+                    <h4 class="h">Flags: {this.props.flags} <br/> Uptime: {this.props.uptime}</h4>
+                </div>
+            )}
+        }
         
 }
-}
+
 export default Task;
